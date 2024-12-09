16e9639004f21cbea5b9d5fde5860c8c66b94f94
KHATRIRAMESH <khatriramesh972@gmail.com>
2024-12-09 15:39:44 +0545

    old password needed while updating admin credentials

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -1,11 +1,10 @@
-import { Button, TextInput } from "flowbite-react";
+import { Button, Label, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import {
   updateSuccess,
   updateFailure,
   updateStart,
-  
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
@@ -15,43 +14,47 @@ const DashProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
 
   const [formData, setFormData] = useState({});
+  console.log(formData)
 
   const dispatch = useDispatch();
-
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
+  console.log("formData", formData);
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (Object.keys(formData).length === 0) {
       return;
     }
+    if (!formData.new_username && !formData.new_password) {
+      return dispatch(updateFailure("Please enter your new username and password"))
+    }
 
     try {
       dispatch(updateStart());
 
       //
-      console.log(currentUser._id)
-      const res = await axiosInstance.put(`api/admin/update/${currentUser._id}`, formData);
+      
+      console.log(currentUser._id);
+      const res = await axiosInstance.put(
+        `api/admin/update/${currentUser._id}`,
+        formData
+      );
       console.log(res.data.message);
 
-
-      if (res.statusText =="OK") {
+      if (res.statusText == "OK") {
         dispatch(updateSuccess(res.data.message));
         // Reset form data
       } else {
         dispatch(updateFailure(res.data.message));
       }
     } catch (error) {
-      
       dispatch(updateFailure(error));
     }
   };
-
- 
 
   return (
     <>
@@ -69,18 +72,34 @@ const DashProfile = () => {
             />
           </div>
           <div className="flex flex-col gap-2 mt-5">
+            {/* <Label value="Current Username:" /> */}
             <TextInput
               type="text"
-              id="username"
-              placeholder="username"
-              defaultValue={currentUser.username}
+              id="old_username"
+              // readOnly={true}
+              // defaultValue={currentUser.username}
+              placeholder="Enter current username..."
               onChange={handleChange}
             />
-            
+            {/* <Label value="Enter new username:" /> */}
             <TextInput
               type="text"
-              id="password"
-              placeholder="Change your password"
+              id="new_username"
+              placeholder="Enter new username..."
+              onChange={handleChange}
+            />
+            {/* <Label value="Enter old password:" /> */}
+            <TextInput
+              type="text"
+              id="old_password"
+              placeholder="Enter your old password..."
+              onChange={handleChange}
+            />
+            {/* <Label value="Enter new password:" /> */}
+            <TextInput
+              type="text"
+              id="new_password"
+              placeholder="Enter your new password..."
               onChange={handleChange}
             />
             <Button type="submit">Update</Button>
@@ -93,12 +112,6 @@ const DashProfile = () => {
             )}
           </div>
         </form>
-        {/* <div className="text-red-500 flex justify-between  mt-3">
-          <span className="cursor-pointer">Delete</span>
-          <span className="cursor-pointer" onClick={handleSignOut}>
-            Sign out
-          </span>
-        </div> */}
       </div>
     </>
   );
